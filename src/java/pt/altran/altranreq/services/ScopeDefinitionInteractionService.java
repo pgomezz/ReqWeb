ad895acc3a83c368a43966e9c5c919baeb9ea67a
RSereno <rafaelsereno@gmail.com>
2013-11-29 16:44:14 +0000

    Update ScopeDefinitionInteractionService.java
    Comment added and some code correction

--- src/java/pt/altran/altranreq/services/ScopeDefinitionInteractionService.java
+++ src/java/pt/altran/altranreq/services/ScopeDefinitionInteractionService.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+//ScopeDefinitionInteraction v2 Implemantion
 package pt.altran.altranreq.services;
 
 import java.util.List;
 import javax.ejb.Local;
 import pt.altran.altranreq.entities.ScopeDefinitionInteraction;
 
-/**
- *
- * @author User
- */
 @Local
 public interface ScopeDefinitionInteractionService {
 
